756620d60a66ef5300c5a0f8a26e563f8e38c293
Dennis Wolff <dennis.costa@alumni.fh-aachen.de>
2024-04-15 17:33:45 +0200

    M1-A9: (Fixed) deleted User model and obsolete code within DatabaseSeeder file.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,10 +15,5 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
         $this->call(DevelopmentData::class); // added by Dennis
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
     }
 }
