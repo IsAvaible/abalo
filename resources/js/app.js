7e16df0229507b63cfed2cfd6aca525656683f04
Simon Conrad <simon.conrad@alumni.fh-aachen.de>
2024-06-06 12:10:35 +0200

    M4-A10: Add basic search bar component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,8 @@
 import './bootstrap';
 import {createApp} from 'vue';
 import HelloVue from "@/components/vue/misc/HelloVue.vue";
+import SearchBar from "@/components/vue/page/SearchBar.vue";
 
 createApp(HelloVue).mount('#hello-vue');
+createApp(SearchBar).mount('#VUE-search-bar');
+
